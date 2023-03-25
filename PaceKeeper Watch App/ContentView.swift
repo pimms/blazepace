e2a988263f53514dd97e41cc50e7f023839059dd
Joakim Stien <joakim.stien@icloud.com>
2023-03-25 09:10:23 +0100

    Initial Commit

---
+++ PaceKeeper Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PaceKeeper Watch App
+//
+//  Created by Joakim Stien on 25/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PaceKeeper Watch App/PaceKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaceKeeperApp.swift
+//  PaceKeeper Watch App
+//
+//  Created by Joakim Stien on 25/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct PaceKeeper_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
